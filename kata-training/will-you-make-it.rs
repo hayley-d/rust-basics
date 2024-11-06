93c49224976d01873aa98d481af3231a2a69351e
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-06 15:43:33 +0200

    Update will-you-make-it.rs

--- kata-training/will-you-make-it.rs
+++ kata-training/will-you-make-it.rs
@@ -9,3 +9,11 @@ fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
 fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
   distance_to_pump <= mpg * gallons
 }
+
+// Improvement
+fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
+    let distance_to_pump_km = distance_to_pump as f32 * 1.609344; // convert to reasonable units first
+    let litres = gallons as f32 * 3.78541178;
+    let km_per_liter = mpg as f32 * 1.609344 / 3.78541178;
+    distance_to_pump_km <= km_per_liter * litres 
+}
