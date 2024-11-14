bfdb882b4b4d771c394ff4c4d326925e41db435d
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-11-14 20:32:43 +0200

    Update high-score-word.rs

--- kata-training/high-score-word.rs
+++ kata-training/high-score-word.rs
@@ -44,3 +44,13 @@ fn high(input: &str) -> &str {
         })
         .unwrap_or("")
 }
+
+fn high(input: &str) -> &str {
+    input.split_ascii_whitespace().rev().max_by_key(|s| s.chars().map(|c| c as u16 - 96).sum::<u16>()).unwrap_or("")
+}
+
+fn high(input: &str) -> &str {
+    input.split_whitespace().min_by_key(|word|
+        - word.chars().map(|c| (c as i32) - 96).sum::<i32>()
+    ).unwrap_or("")
+}
